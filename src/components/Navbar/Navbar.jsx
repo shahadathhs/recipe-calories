0fd89b7b40fb1213cc4a5ff5bf45319a82605609
Shahadath Hossen Sajib <shahadathhossensajib732@gmail.com>
2024-03-16 09:30:17 +0600

    navbar completed

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import { CgProfile } from "react-icons/cg";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </div>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-gray-500">
+              <li><a>Home</a></li>
+              <li><a>Recipes</a></li>
+              <li><a>About</a></li>
+              <li><a>Search</a></li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl font-bold lg:text-3xl lg:font-extrabold">Recipe Calories</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 text-gray-500">
+            <li><a>Home</a></li>
+            <li><a>Recipes</a></li>
+            <li><a>About</a></li>
+            <li><a>Search</a></li>
+          </ul>
+        </div>
+        {/* navbar end */}
+        <div className="navbar-end gap-4">
+          <div className="form-control hidden md:flex lg:flex">
+            <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto rounded-full" />
+          </div>
+          <a className="btn rounded-full bg-green-500"><CgProfile /></a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
